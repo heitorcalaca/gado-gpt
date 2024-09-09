06574c6277fa62bacf2ceb846d8988b10203d64f
Heitor Calaça <heitor.calaca@gmail.com>
2024-09-09 13:07:14 -0300

    .

--- src/app/api/desmama/route.js
+++ src/app/api/desmama/route.js
@@ -1,10 +1,23 @@
 // src/app/api/desmama/route.js
+import { authOptions } from "@/lib/authOption";
 import connectToDatabase from "@/lib/mongoose";
 import Filhote from "@/models/Filhote";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   await connectToDatabase();
+
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json(
+      { message: "Não autorizado. Por favor, faça login." },
+      { status: 401 }
+    );
+  }
+
+  const user = session.user;
 
   // Data atual menos 9 meses
   const noveMesesAtras = new Date();
@@ -14,6 +27,7 @@ export async function GET() {
   const filhotesProntosParaDesmama = await Filhote.find({
     dataNascimento: { $lte: noveMesesAtras },
     situacao: "NO", // Considerando que "NO" é a situação para filhotes não desmamados
+    userId: user.id,
   })
     .populate("matriz")
     .lean();
